bdad97fc6d1aeb14aeb0475e6786b0e6f3bb595f
Teo <teoirado@gmail.com>
2023-07-20 01:19:59 -0300

    types

---
+++ src/types/Repository.ts
@@ -0,0 +1,7 @@
+export type RepositoryProps = {
+  id: number;
+  html_url: string;
+  name: string;
+  language: string;
+  updated_at: string;
+};
---
+++ src/types/User.ts
@@ -0,0 +1,15 @@
+export type UserProps = {
+  login: string;
+  name: string;
+  avatar_url: string;
+  bio: string;
+  company: string;
+  email: string;
+  location: string;
+  html_url: string;
+  followers: number;
+  followers_url: string;
+  following: number;
+  following_url: string;
+  repos_url: string;
+};
